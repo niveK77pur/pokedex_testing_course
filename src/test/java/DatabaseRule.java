080953d2d1b90f92cde19240c5221a52a04239e7
niveK77pur <kevinbiewesch@yahoo.fr>
2021-04-22 19:13:57 +0200

    fix database access

--- src/test/java/DatabaseRule.java
+++ src/test/java/DatabaseRule.java
@@ -9,7 +9,7 @@ public class DatabaseRule extends ExternalResource {
 
   @Override
   protected void before() {
-    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/pokedex_test", null, null);
+    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/pokedex_test", "postgres", "yamizi");
   }
 
   @Override
